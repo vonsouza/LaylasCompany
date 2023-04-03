7c7fb2908900cd23fb8764b46be06f5f9d238243
Von Souza <von.souza@impactzero.pt>
2023-04-03 22:45:31 +0100

    Best sellers up. Main index with 4 items. Empty cart message.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData("best-sellers");
 const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
+const listing = new ProductList("Best-Sellers", dataSource, element);
 
 listing.init();
